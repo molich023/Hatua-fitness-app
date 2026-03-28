8befe3a5ab616f38cdf0ed281d117ffb5c965588
Molich <molich60@gmail.com>
2026-03-29 00:45:52 +0300

    Add translations for English and Swahili

---
+++ src/lib/translations.ts
@@ -0,0 +1,18 @@
+{
+  "en": {
+    "welcome": "Every step is a small victory",
+    "start": "START JOURNEY",
+    "stop": "STOP TRACKING",
+    "points": "Hatua Points",
+    "distance": "Distance (KM)",
+    "referral_msg": "Share your code and earn 20 points!"
+  },
+  "sw": {
+    "welcome": "Kila hatua ni ushindi mdogo",
+    "start": "ANZA SAFARI",
+    "stop": "ACHA KUFUATILIA",
+    "points": "Pointi za Hatua",
+    "distance": "Umbali (KM)",
+    "referral_msg": "Shiriki msimbo wako upate pointi 20!"
+  }
+}
