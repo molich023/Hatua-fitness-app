3ff45b420a3a17ce44ee0f55977dac542fe0ff82
Molich <molich60@gmail.com>
2026-03-30 23:34:59 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'HATUA Fitness',
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'HATUA',
+  },
+};
