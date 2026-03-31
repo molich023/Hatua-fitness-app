52b97f36339fb99842825a0e5c6b1d8c18d351db
Molich <molich60@gmail.com>
2026-03-31 07:40:53 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+ import { Toaster } from 'sonner';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-gray-50 text-slate-900">
-        <SessionProvider>
-          {children}
-        </SessionProvider>
+      <body>
+        {children}
+        {/* This is the container for all popups */}
+        <Toaster position="top-center" expand={true} richColors />
       </body>
     </html>
   );
 }
+
